95ad4f74442e747f9c884ebb11f3b0474eaedcf4
Vivek Chandra Sardar <chandravivek52@gmail.com>
2024-01-03 19:56:24 +0530

    create modals-component...

---
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mods-home',
+  templateUrl: './mods-home.component.html',
+  styleUrls: ['./mods-home.component.css']
+})
+export class ModsHomeComponent {
+
+  modalOpen = false;
+
+  modalHandler() {
+    this.modalOpen = !this.modalOpen;
+  }
+}
